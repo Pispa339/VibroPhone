c931a6948c48423dd386eec8649518b5d4277f0d
Pispa39 <pispa.juho.k@student.uta.fi>
2016-03-11 16:51:43 +0200

    Creating messages implemented, working on receiving from database and playing them

---
+++ VibroPhone/MessageViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MessageViewController.swift
+//  VibroPhone
+//
+//  Created by Juho Pispa on 11.3.2016.
+//  Copyright © 2016 Juho Pispa. All rights reserved.
+//
+
+import Cocoa
+
+class MessageViewController: UIViewController {
+
+}
